5d215254d8ec24df213f8efedf686adc3deb7989
EC2 Default User <ec2-user@ip-172-31-53-194.ec2.internal>
2021-02-26 03:02:20 +0000

    Got the winner funnction to print out if a player has won. I still need to make it so the browsers aren't tp click any button

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
     const [Player1, setArray1] = useState([]);
     const [Player2, setArray2] = useState([]);
     const [hideSpec, setSpectator] = useState(true);
+    const winner = calculateWinner(board);
     const inputRef = useRef(null);
 
 
@@ -25,7 +26,7 @@ function App() {
         console.log("Players turn");
         console.log("Player 1",player1,"Player2", player2);
         console.log(player);
-        console.log(spectators);
+        //console.log(spectators);
 
         setCount((prevCount) => prevCount + itmNum);
         var letter = "";
@@ -34,16 +35,18 @@ function App() {
         if(counter % 2 == 0) {
             letter = "X";
             board[index] = letter;
-            setArray1((prevArray) => [...prevArray, index]);
-            console.log(Player1);
+            Player1.push(index);
+            //console.log(Player1);
+            calculateWinner(Player1);
             //player1moves.push(index);
         }
+        
         else{
             letter = "O";
             board[index] = letter;
-            setArray2((prevArray1) => [...prevArray1, index]);
-            //calculateWinner(Player2);
-            console.log(Player2);
+            Player2.push(index);
+            //console.log(Player2);
+            calculateWinner(Player2);
             //player2moves.push(index)
             }
         console.log(counter);
@@ -72,14 +75,13 @@ function App() {
         var i;
         var spectators = []
         for(i=2; i < connected.length;i++){
-            spectators.push(connected[spectators]);
+            spectators.push(connected[i]);
         }
         
         for(i=0; i < spectators.length; i++){
-        setSpectator(prevSpec => spectators[i]);
-        isSpectator(hideSpec);
+            isSpectator(spectators[i]);
         
-        return spectators;
+           // return spectators;
         }
     }
 
@@ -88,6 +90,7 @@ function App() {
         if (username != "") {
             loggedin = true;
             setConnect(prevConn => [...prevConn,username]);
+            
             socket.emit('Logins', { joined: username });
             showBoard(); // shows the board if you click on login
             console.log(username);
@@ -113,7 +116,8 @@ function App() {
     }
 
     function calculateWinner(player) {
-        const lines = [
+        console.log(player);
+        const possible = [
             [0, 1, 2],
             [3, 4, 5],
             [6, 7, 8],
@@ -123,9 +127,10 @@ function App() {
             [0, 4, 8],
             [2, 4, 6],
         ];
-        for (let i = 0; i < lines.length; i++) {
-            const [a, b, c] = lines[i];
+        for (let i = 0; i < possible.length; i++) {
+            const [a, b, c] = possible[i];
             if (player[a] && player[a] === player[b] && player[a] === player[c]) {
+                console.log("You are a winner");
                 return player[a];
             }
         }
@@ -185,7 +190,9 @@ function App() {
             board.map(
                 (itm, idx) => 
                 <div class="box">
+                    {hideSpec === true ? (
                     <button onClick={() => onClickButton(turn(player1,player2),idx,1)}> Click </button>
+                     ) : null}
                     <Tboard letter={itm}/>
                 </div>
              )
