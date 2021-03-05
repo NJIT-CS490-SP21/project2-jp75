b223f33234b697ce7afb75ed58505e27e176008d
EC2 Default User <ec2-user@ip-172-31-53-194.ec2.internal>
2021-03-05 04:50:51 +0000

    Made the database updateable... When someone wins they get +1 if they lose they get -1. There is a bug in my code that sometimes it doesn't update the database all the time... its around a 50% adding rate

--- app.py
+++ app.py
@@ -31,16 +31,14 @@ socketio = SocketIO(
 win = []
 loss = []
 tie = []
-
-
 logins = []
 Player1 = ""
 Player2 = ""
 Spectators = []
 players = []
-users = {"PlayerX":"","PlayerO":"","Spectators":[]}
+users = {"PlayerX":"", "PlayerO":"", "Spectators":[]}
 
-@app.route('/', defaults={"filename": "index.html"})
+@app.route('/', defaults={"filename":"index.html"})
 @app.route('/<path:filename>')
 def index(filename):
     return send_from_directory('./build', filename)
@@ -64,14 +62,12 @@ def on_Connection1(data):
     global Player2
     print(str(data))
     logins.append(str(data['joined']))
-    print(logins);
-    
+    print(logins)
     db_names = []
     db_scores = []
-    
-    new_user = models.Joined(username=data['joined'],score=100)
-    print("New user",new_user)
-    
+    new_user = models.Joined(username=data['joined'], score=100)
+    print("New user", new_user)
+    #we need to see if the user already exists in the database
     exists = bool(models.Joined.query.filter_by(username=data['joined']).first())
     #print(exists)
     if exists != True:              #gets if user is already in db
@@ -79,12 +75,9 @@ def on_Connection1(data):
         db.session.commit()
         
     all_people = models.Joined.query.all()
-    
-
     for People in all_people:
         db_names.append(People.username) #appends username to database
         db_scores.append(People.score)
-    
     socketio.emit("User_List", {'users': db_names, 'score': db_scores}) #this might be a problem everytime
     
     if(Player1 == ""):
@@ -105,34 +98,52 @@ def on_Connection1(data):
         users["PlayerX"] = players[0]
     elif(users["PlayerO"] == ""):
         users["PlayerO"] = players[1]
-        
     else:
         users["Spectators"] = Spectators
-
-
     print(users)
-    
-    
+
     socketio.emit("Logins", data, broadcast=True, include_self=True)
-    
-    
-    print(Player1,"is X")
+
+    print(Player1, "is X")
     print(Player2, "is O")
     print(Spectators, " are spectating the game")
-
 
 @socketio.on('Reset')
 def reset(data):
     print(str(data))
-   
-    socketio.emit('Reset', data , broadcast=True, include_self=True) 
-    
+    socketio.emit('Reset', data, broadcast=True, include_self=True) 
     
 @socketio.on('Winner')
 def winner(data):
     global win
     global loss
     print(str(data))
+    users = []
+    scores = []
+    winner = models.Joined.query.filter_by(username=data['winner']).first()
+    loser = models.Joined.query.filter_by(username=data['loser']).first()
+    
+    print("Winner username: ",winner.username," Score: ",winner.score)
+    print("Loser username: ",loser.username," Score: ",loser.score)
+   # print("After:")
+    winner.score = winner.score + 1
+    db.session.merge(winner)
+    loser.score = loser.score - 1
+    db.session.merge(loser)
+    db.session.commit()
+    print("Winner username: ",winner.username," Score: ",winner.score)
+    print("Loser username: ",loser.username," Score: ",loser.score)
+
+    
+    all_people = models.Joined.query.all()
+    for people in all_people:
+        users.append(people.username)
+        scores.append(people.score)
+    socketio.emit("User_List", {'users': users, 'score': scores})
+        
+    print(users)
+    print(scores)
+    
     win.append(str(data['winner']))
     print(win)
     loss.append(str(data['loser']))
@@ -140,15 +151,12 @@ def winner(data):
     
     socketio.emit('Winner', data, broadcast=True, include_self=False)
     
-    
 @socketio.on('Draw')
 def draw(data):
     global tie
     print(str(data))
     tie.append(str(data['Draw']))
-    
-    print(tie)
-    
+    #print(tie)
     socketio.emit('Draw', data, broadcast=True, include_self=False)
 # When a client emits the event 'chat' to the server, this function is run
 # 'chat' is a custom event name that we just decided
@@ -156,15 +164,14 @@ def draw(data):
 def on_click(data): # data is whatever arg you pass in your emit call on client
     print(str(data))
     
-    
     # This emits the 'chat' event from the server to all clients except for
     # the client that emmitted the event that triggered this function
-    socketio.emit('Play', data , broadcast=True, include_self=False)
-if __name__ =='__main__':
+    socketio.emit('Play', data, broadcast=True, include_self=False)
+if __name__ == '__main__':
 # Note that we don't call app.run anymore. We call socketio.run with app arg
     socketio.run(
         app,
         host=os.getenv('IP', '0.0.0.0'),
         port=8081 if os.getenv('C9_PORT') else int(os.getenv('PORT', 8081)),
-        debug = True
+        debug=True
     )
