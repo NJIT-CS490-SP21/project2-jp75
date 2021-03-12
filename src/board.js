c9151e9d76c923e33984e32160b1f288b1fe5702
EC2 Default User <ec2-user@ip-172-31-53-194.ec2.internal>
2021-03-12 05:56:05 +0000

    working with airbnb to dploy heroku

--- src/board.js
+++ src/board.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React from 'react';
 
 export function Tboard(props) {
-  return <div> {props.letter} </div>;
+  return (
+    <div>
+      {props.letter}
+    </div>
+  );
 }
 export function User(props) {
-  return <div> {props.name}</div>;
+  return (
+    <div>
+      {props.name}
+    </div>
+  );
 }
 
 export function LoginButton(props) {
-  return <button onClick={props.onClick}>Login</button>;
+  return (
+    <button onClick={props.onClick}>Login</button>
+  );
 }
